9a3f5dca63bad699bfb1b200ca1e9c63c3407d67
Jimmy Sené <jimmy.sene@laposte.net>
2018-02-13 11:30:52 +0100

    Initialisation de l'app

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'help',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'heLP';
+}
